cbf236c2caa6abd0f1890d39cac871a62e33b4f5
Chimwemwe-127001 <sinyinzachimwemwe@gmail.com>
2022-03-08 17:40:19 +0200

    Add logic to save and convert data to_json

--- book.rb
+++ book.rb
@@ -12,4 +12,16 @@ class Book
   def add_rental(date, person)
     Rental.new(date, self, person)
   end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      :title => @title,
+      :author => @author
+    }.to_json(*args)
+  end
+
+  def self.from_json(obj)
+    new(obj['title'], obj['author'])
+  end
 end
